8de5eab7ecb4b8de9750f3d4ce487d04cf7267b5
Brian Ho <ho_brian@me.com>
2017-12-01 22:30:44 -0500

    Update geo files

--- js/map.js
+++ js/map.js
@@ -14,17 +14,6 @@
   	.style("visibility", "hidden")
   	.text("a simple tooltip");
 
-// countriesGroup = svg
-//     .append("g")
-//     .attr("id", "map");
-// // add a background rectangle
-// countriesGroup
-//     .append("rect")
-//     .attr("x", 0)
-//     .attr("y", 0)
-//     .attr("width", $("#map-holder").width())
-//     .attr("height", $("#map-holder").height());
-
 d3.queue()
   .defer(d3.json, "geo/circle_rad_5km.geojson")
   // .defer(d3.json, "geo/mass_roads_5km.json")
@@ -41,23 +30,11 @@ function makeMyMap(error, circle, boundary, kl){
   var projection = d3.geoConicConformal()
     .parallels([41 + 43 / 60, 42 + 41 / 60])
     .rotate([71 + 30 / 60, -41])
-    .fitExtent([[0,0],[width,height]], circle)
-    ;
-    // console.log(projection.scale(), projection.center())
+    .fitExtent([[0,0],[width,height]], circle);
   var path = d3.geoPath().projection(projection);
-
-  // console.log(outer);
-  // svg.selectAll(".outer")
-  //     .data(outer.features)
-  //     .enter()
-  //     .append("path")
-  //     .attr("d", path)
-  //     .attr("stroke-width", 1)
-  //     .attr("stroke","white")
-  //     .attr("fill","grey");
+  console.log(projection.scale(), projection.center());
 
   svg.selectAll(".boundary")
-      // .data(boundary.features)
       .data(topojson.feature(boundary, boundary.objects.City_of_Boston_Boundary).features)
       .enter()
       .append("path")
