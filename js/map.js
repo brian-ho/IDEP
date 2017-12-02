536f352aa25398bfb3f9b5f08faf20e4bad08783
Brian Ho <ho_brian@me.com>
2017-12-02 17:58:04 -0500

    Formatting and cleanup.

--- js/map.js
+++ js/map.js
@@ -18,7 +18,6 @@ d3.queue()
   // .defer(d3.json, "geo/circle_7_5km.geojson")
   // // .defer(d3.json, "geo/circle_10km.geojson")
   .defer(d3.json, "geo/circle_15km.geojson")
-  .defer(d3.json, "geo/coastline_20km.geojson")
   .defer(d3.json, "geo/hydro.geojson")
   .defer(d3.json, "geo/City_of_Boston_Boundary.json")
   .defer(d3.csv, "data/kl_geocode_2g.csv")
@@ -26,7 +25,7 @@ d3.queue()
 
 
 // d3.json("geo/City_of_Boston_Boundary.geojson", function(error, data) {
-function makeMyMap(error, circle, circle2, coast, hydro, boundary, kl){
+function makeMyMap(error, circle, circle2, hydro, boundary, kl){
   if (error) throw error;
 
   // NAD83 Massachusetts Mainalnd (EPSG:26986)
@@ -40,14 +39,10 @@ function makeMyMap(error, circle, circle2, coast, hydro, boundary, kl){
   console.log(projection.scale(), projection.center());
 
   var zoom = d3.zoom()
-      // no longer in d3 v4 - zoom initialises with zoomIdentity, so it's already at origin
-      // .translate([0, 0])
-      // .scale(1)
-      .scaleExtent([1, 8])
+      .scaleExtent([1, 15]) // This is a relative zoom level (go figure)
       .on("zoom", zoomed);
 
  svg.call(zoom)
-  .on("dblclick", function() { d3.event.preventDefault(); })
   .on("wheel", function() { d3.event.preventDefault(); });
 
   svg.append("rect")
@@ -56,15 +51,16 @@ function makeMyMap(error, circle, circle2, coast, hydro, boundary, kl){
     .attr("height", height)
     .on("click", reset);
 
-  var g = svg.append('g');
-
-  g.selectAll(".boundary")
-      .data(topojson.feature(boundary, boundary.objects.City_of_Boston_Boundary).features)
-      .enter()
-      .append("path")
-      .attr("d", path)
-      .attr("stroke-width", .125)
-      .attr("stroke","white");
+  var g = svg.append('g')
+    .attr("class", "geometry");
+  //
+  // g.selectAll(".boundary")
+  //     .data(topojson.feature(boundary, boundary.objects.City_of_Boston_Boundary).features)
+  //     .enter()
+  //     .append("path")
+  //     .attr("d", path)
+  //     .attr("stroke-width", .125)
+  //     .attr("stroke","white");
 
   g.selectAll(".circle")
       .data(circle.features)
@@ -76,38 +72,21 @@ function makeMyMap(error, circle, circle2, coast, hydro, boundary, kl){
       .attr("stroke", "white")
       .attr("stroke-width", .125);
 
-  g.selectAll(".coast")
-      .data(coast.features)
-      // .data(topojson.feature(roads, roads.objects.mass_roads_5km).features)
+  g.selectAll(".hydro")
+      .data(hydro.features)
+      // .data(topojson.feature(hydro, hydro.objects.hydro_20km_dissolve).features)
       .enter()
       .append("path")
       .attr("d", path)
-      .attr("fill", "none")
+      // .attr("fill", "grey")
       .attr("stroke", "white")
       .attr("stroke-width", .125);
 
-    g.selectAll(".hydro")
-        .data(hydro.features)
-        // .data(topojson.feature(hydro, hydro.objects.hydro_20km_dissolve).features)
-        .enter()
-        .append("path")
-        .attr("d", path)
-        // .attr("fill", "grey")
-        .attr("stroke", "white")
-        .attr("stroke-width", .125);
-  // svg.selectAll(".circle2")
-  //     .data(circle2.features)
-  //     // .data(topojson.feature(roads, roads.objects.mass_roads_5km).features)
-  //     .enter()
-  //     .append("path")
-  //     .attr("d", path)
-  //     .attr("stroke", "white")
-  //     .attr("stroke-width", .125);
-
-  g.selectAll("circles")
+  g.selectAll(".dots")
       .data(kl)
       .enter()
       .append("circle")
+      .attr("class", "dots")
       .attr("r", 2)
       .attr("cx", function(d) {return projection([d.lng0, d.lat0])[0]} )
       .attr("cy", function(d) {return projection([d.lng0, d.lat0])[1]} )
@@ -137,7 +116,10 @@ function reset() {
 
 
 function zoomed() {
-  // g.style("stroke-width", 1.5 / d3.event.transform.k + "px");
-  g.attr("transform", d3.event.transform); // updated for d3 v4
+  g.style("stroke-width", 1.5 / d3.event.transform.k + "px");
+  g.attr("transform", d3.event.transform);
+  d3.selectAll(".dots")
+    .attr("r", 2 / d3.event.transform.k + "px");
+    // updated for d3 v4
   }
 }
