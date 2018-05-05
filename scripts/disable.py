4d15f26fbf72b3bc7d339c3e49213b8f751e45ed
Brian Ho <ho_brian@me.com>
2018-05-05 15:26:38 -0400

    Scripts to batch MT tasks

--- scripts/disable.py
+++ scripts/disable.py
@@ -5,6 +5,7 @@ from boto.mturk.connection import MTurkConnection
 from boto.mturk.question import ExternalQuestion
 from boto.mturk.qualification import Qualifications, PercentAssignmentsApprovedRequirement, NumberHitsApprovedRequirement
 from boto.mturk.price import Price
+import pandas as pd
 import sys
 
 hit = sys.argv[1]
@@ -13,13 +14,32 @@ hit = sys.argv[1]
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
-if len(sys.argv) > 2 and sys.argv[2] == 'pub':
-    AMAZON_HOST = "mechanicalturk.amazonaws.com"
+if  hit == 'csv':
+    csv_path = sys.argv[2]
+    df = pd.read_csv(csv_path)
+
+    if len(sys.argv) > 3 and sys.argv[3] == 'pub':
+        AMAZON_HOST = "mechanicalturk.amazonaws.com"
+    else:
+        AMAZON_HOST = "mechanicalturk.sandbox.amazonaws.com"
+
+    connection = MTurkConnection(aws_access_key_id=AWS_ACCESS_KEY_ID,
+                                 aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                                 host=AMAZON_HOST)
+
+    for i, hit in df.iterrows():
+        connection.disable_hit(hit.hitID)
+        print 'Disabling HIT: %s' % hit.hitID
+
 else:
-    AMAZON_HOST = "mechanicalturk.sandbox.amazonaws.com"
+    if len(sys.argv) > 2 and sys.argv[2] == 'pub':
+        AMAZON_HOST = "mechanicalturk.amazonaws.com"
+    else:
+        AMAZON_HOST = "mechanicalturk.sandbox.amazonaws.com"
 
-connection = MTurkConnection(aws_access_key_id=AWS_ACCESS_KEY_ID,
-                             aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-                             host=AMAZON_HOST)
+    connection = MTurkConnection(aws_access_key_id=AWS_ACCESS_KEY_ID,
+                                 aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                                 host=AMAZON_HOST)
 
-connection.disable_hit(hit)
+    connection.disable_hit(hit)
+    print 'Disabling HIT: %s' % hit
--- scripts/post_hits.py
+++ scripts/post_hits.py
@@ -6,6 +6,7 @@ from boto.mturk.question import ExternalQuestion
 from boto.mturk.qualification import Qualifications, PercentAssignmentsApprovedRequirement, NumberHitsApprovedRequirement
 from boto.mturk.price import Price
 import sys
+import pandas as pd
 
 task = sys.argv[1]
 
@@ -25,14 +26,21 @@ connection = MTurkConnection(aws_access_key_id=AWS_ACCESS_KEY_ID,
 
 #5 cents per HIT
 if task == 'guess':
+    title = 'Help locate street images of Boston! You can take multiple times. IDEP'
     amount = 0.05
 elif task == 'label':
+    title = 'Help label street images of Boston! You can take multiple times. IDEP'
     amount = 0.05
 elif task == 'share':
+    title = 'Help share your memories of Boston! You can take multiple times. IDEP'
     amount = 0.01
 else:
+    title = 'Help make a city image for Boston! You can take multiple times. IDEP'
     amount = 0.01
 
+totalHITS = 100
+urls = []
+hitIDs = []
 #frame_height in pixels
 frame_height = 1200
 #Here, I create two sample qualifications
@@ -43,27 +51,35 @@ qualifications = Qualifications()
 #This url will be the url of your application, with appropriate GET parameters
 url = "https://new-city-image.herokuapp.com/%s" % (task)
 questionform = ExternalQuestion(url, frame_height)
-create_hit_result = connection.create_hit(
-    title="Help identify street images of Boston (IDEP)!",
-    description="Participate in a short survey about perception of images of cities!",
-    keywords=["city", "perception", "quick"],
-    #duration is in seconds
-    duration = 60*5,
-    #max_assignments will set the amount of independent copies of the task (turkers can only see one)
-    max_assignments=100,
-    question=questionform,
-    reward=Price(amount=amount),
-     #Determines information returned by method in API, not super important
-    response_groups=('Minimal', 'HITDetail'),
-    qualifications=qualifications,
-    )
 
-# The response included several fields that will be helpful later
-hit_type_id = create_hit_result[0].HITTypeId
-hit_id = create_hit_result[0].HITId
-print "Your HIT has been created. You can see it at this link:"
-if len(sys.argv) > 2 and sys.argv[2] == 'pub':
-    print "https://www.mturk.com/mturk/preview?groupId={}".format(hit_type_id)
-else:
-    print "https://workersandbox.mturk.com/mturk/preview?groupId={}".format(hit_type_id)
-print "Your HIT ID is: {}".format(hit_id)
+for HIT in range(totalHITS):
+    create_hit_result = connection.create_hit(
+        title=title,
+        description="Participate in a short survey about perception of images of cities!",
+        keywords=["city", "perception", "quick"],
+        #duration is in seconds
+        duration = 60*5,
+        #max_assignments will set the amount of independent copies of the task (turkers can only see one)
+        max_assignments=10,
+        question=questionform,
+        reward=Price(amount=amount),
+         #Determines information returned by method in API, not super important
+        response_groups=('Minimal', 'HITDetail'),
+        qualifications=qualifications,
+        )
+
+    # The response included several fields that will be helpful later
+    hit_type_id = create_hit_result[0].HITTypeId
+    hit_id = create_hit_result[0].HITId
+
+    if len(sys.argv) > 2 and sys.argv[2] == 'pub':
+        url_preview = "https://www.mturk.com/mturk/preview?groupId={}".format(hit_type_id)
+    else:
+        url_preview = "https://workersandbox.mturk.com/mturk/preview?groupId={}".format(hit_type_id)
+
+    print "ID is: {} - ".format(hit_id), url_preview
+    urls.append(url_preview)
+    hitIDs.append(hit_id)
+
+df = pd.DataFrame({'hitID':hitIDs, 'hitURL':urls})
+df.to_csv('logs/HITs.csv')
